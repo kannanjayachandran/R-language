24abbb3424f38694684fc7aaa685025838e2f7eb
kannan jayachandran <81518270+kannanjayachandran@users.noreply.github.com>
2021-11-05 20:41:12 +0530

    first

---
+++ 2. Practice/intro.r
@@ -0,0 +1,51 @@
+
+# Storing the numbers as a vector.
+# c()  is the combine function. it returns a vector (one dimensional array)
+x = c(9, 16, 36, 49, 64, 65, 81, 100)
+
+# length()  will give the length
+length(x)
+
+# max()   will give the maximum among the numbers.
+max(x)
+
+# min()   will give the minimum among the numbers.
+min(x)
+
+# sqrt()   will give the square root
+sqrt(x)
+
+#  Round off
+round(sqrt(x))
+
+# floor round off / integer round off.
+floor(sqrt(x))
+
+#  Sum of all the members.
+sum(x)
+
+#  Product of all members
+prod(x)
+
+#  Exponent of all
+exp(x)
+
+#  log
+log(x)
+
+# common logarithm ie; logarithm with base 10. 
+log10(x)
+
+#  Binary logarithm
+log2(x)
+
+# summary  :  min, max, median, 3rd Quartile.
+summary(x)
+
+# Histogram plot
+hist(x)
+
+# Box plot
+boxplot(x)
+
+
